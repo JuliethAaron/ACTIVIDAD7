e23c6d96dfb769a1b3031ae8903acf42c6342cc9
JuliethAaron <jaaronibanez@gmail.com>
2020-10-17 21:09:19 -0500

    Tipos de Aprendizajes

---
+++ Actividad 4/script.js
@@ -0,0 +1,33 @@
+/* ID */
+ $('#parrafo').css("background", "red")
+
+ $ ('.mensaje').css("font-size" , "30px").
+ css("background", "blue")
+ $('#bonito').click(function () {
+     $(this).css("font-size" , "50px")
+     
+ })
+$('#grandes').dblclick(function () {
+    $(this).css("color" , "orange")
+    
+})
+$('li').mouseover(function () {
+    $(this).css("background" , "brown")
+    
+})
+
+$('li').mouseout(function () {
+    $(this).css("background" ,"yellow")
+    
+})
+$('div').click(function () {
+    $(this).css("background" ,"pink")
+})
+
+$('div').mouseout(function () {
+    $(this).css("background" ,"purple")
+})
+
+$('div').mouseover(function () {
+    $(this).css("background" ,"orange")
+})
